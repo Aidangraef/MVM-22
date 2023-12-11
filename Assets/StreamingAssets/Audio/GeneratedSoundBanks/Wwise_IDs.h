b9c6bf5e30db9685ce541e58aea9d544fe76e431
KhaoKG <Evanseamans@gmail.com>
2023-12-11 13:11:21 -0800

    Added Music!

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,26 +14,78 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AMBIENTSTART = 1818308409U;
+        static const AkUniqueID BOSSDEFEATED = 2305261964U;
+        static const AkUniqueID BOSSSTART = 2978173602U;
+        static const AkUniqueID BUTTONCLICK = 4051332235U;
+        static const AkUniqueID FIRSTUPGRADE = 492523291U;
         static const AkUniqueID FLYERATTACK = 1292600457U;
         static const AkUniqueID FLYERDIE = 325523121U;
         static const AkUniqueID FLYERHURT = 4120941918U;
+        static const AkUniqueID FOURTHUPGRADE = 2663107407U;
+        static const AkUniqueID GAMESTART = 4058101365U;
         static const AkUniqueID GRAVEND = 2853039794U;
         static const AkUniqueID GRAVSTART = 1983228305U;
         static const AkUniqueID GRAVVAC = 2230252431U;
+        static const AkUniqueID MUSICSTART = 1122283870U;
         static const AkUniqueID PLAYERDASH = 2525052962U;
         static const AkUniqueID PLAYERJUMP = 4008126242U;
         static const AkUniqueID PLAYERLAND = 846198821U;
         static const AkUniqueID PLAYERMOVE = 2267600265U;
         static const AkUniqueID PLAYERSHIELD = 2594838977U;
+        static const AkUniqueID PLAYERSTOPMOVE = 1563041841U;
+        static const AkUniqueID SECONDUPGRADE = 2826329895U;
         static const AkUniqueID SWING1 = 2597336422U;
         static const AkUniqueID SWING2 = 2597336421U;
         static const AkUniqueID SWING3 = 2597336420U;
         static const AkUniqueID TESTCOMPLETE = 1776921608U;
         static const AkUniqueID TESTFAIL = 1435302941U;
+        static const AkUniqueID THIRDUPGRADE = 4143628668U;
+        static const AkUniqueID TOMENU = 3387993871U;
+        static const AkUniqueID UPGRADEGET = 2612649333U;
         static const AkUniqueID WALKERATTACK = 819226263U;
         static const AkUniqueID WALKERDIE = 4099529891U;
         static const AkUniqueID WALKERHURT = 3105797008U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEZONE
+        {
+            static const AkUniqueID GROUP = 1687686957U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CREDITS = 2201105581U;
+                static const AkUniqueID FIRSTBOSS = 3080698126U;
+                static const AkUniqueID FIRSTZONE = 2254719051U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAMEZONE
+
+        namespace PLAYERUPGRADES
+        {
+            static const AkUniqueID GROUP = 3105312269U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FIRSTUPGRADE = 492523291U;
+                static const AkUniqueID FOURTHUPGRADE = 2663107407U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SECONDUPGRADE = 2826329895U;
+                static const AkUniqueID THIRDUPGRADE = 4143628668U;
+                static const AkUniqueID ZEROUPGRADE = 3089372025U;
+            } // namespace STATE
+        } // namespace PLAYERUPGRADES
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAMEMASTERVOLUME = 4103204207U;
+        static const AkUniqueID GAMEMUSICVOLUME = 516124850U;
+        static const AkUniqueID GAMESFXVOLUME = 1052786170U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
